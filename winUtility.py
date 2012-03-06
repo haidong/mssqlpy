64034146b5a5bcb8a16413264122b4e4611e7420
Haidong Ji <Haidong.Ji@gmail.com>
2012-03-06 13:51:42 -0600

    1st version of getServiceStartupAccount

--- winUtility.py
+++ winUtility.py
@@ -1,4 +1,4 @@
-import shlex, subprocess, sys
+import re, shlex, subprocess, sys
 from datetime import datetime
 """Haidong's class of Windows-specific utilities. It uses sysinternal tools fairly extensively. This is written mostly for SQL Server administration"""
 def runCmd(cmd):
@@ -6,3 +6,23 @@ def runCmd(cmd):
 	out, err = proc.communicate()
 	ret = proc.returncode
 	return (ret, out, err)
+
+def getServiceStartupAccount(serviceName, serverName):
+	serviceName = re.compile(r'SERVICE_NAME:\s%s.*$' % serviceName, re.IGNORECASE)
+	startupAccount = re.compile(r'\tSERVICE_START_NAME:\s(.+)')
+
+	returnCode, stdOut, stdErr = runCmd("""psservice \\\\%s config""" % serverName.strip())
+	targetServiceFound = False
+
+	for line in stdOut.split('\n'):
+		if targetServiceFound:
+			reResult = startupAccount.search(line)
+			if reResult:
+				return reResult.group(1)
+		else:
+			reResult = serviceName.search(line)
+			if reResult:
+				targetServiceFound = True
+	
+	if not targetServiceFound:
+		return None
