0b25719cf9be9c0a4f158cbf0595836c1a0cfd70
Haidong Ji <Haidong.Ji@gmail.com>
2012-03-06 18:17:18 -0600

    Successfully getting win Dhcp service startup account

--- winUtility.py
+++ winUtility.py
@@ -2,23 +2,24 @@ import re, shlex, subprocess, sys
 from datetime import datetime
 """Haidong's class of Windows-specific utilities. It uses sysinternal tools fairly extensively. This is written mostly for SQL Server administration"""
 def runCmd(cmd):
-	proc = subprocess.Popen(shlex.split(cmd), stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
+	proc = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
 	out, err = proc.communicate()
 	ret = proc.returncode
 	return (ret, out, err)
 
-def getServiceStartupAccount(serviceName, serverName):
-	serviceName = re.compile(r'SERVICE_NAME:\s%s.*$' % serviceName, re.IGNORECASE)
+def getServiceStartupAccount(service, serverName):
+	serviceName = re.compile(r'SERVICE_NAME:\s%s.*$' % service, re.IGNORECASE)
 	startupAccount = re.compile(r'\tSERVICE_START_NAME:\s(.+)')
 
-	returnCode, stdOut, stdErr = runCmd("""psservice \\\\%s config""" % serverName.strip())
+	cmd = """psservice \\\\%s config""" % serverName.strip()
+	returnCode, stdOut, stdErr = runCmd(cmd)
 	targetServiceFound = False
 
 	for line in stdOut.split('\n'):
 		if targetServiceFound:
 			reResult = startupAccount.search(line)
 			if reResult:
-				return reResult.group(1)
+				return reResult.group(1).strip()
 		else:
 			reResult = serviceName.search(line)
 			if reResult:
--- winUtilityTest.py
+++ winUtilityTest.py
@@ -8,3 +8,5 @@ class winUtilityTest(unittest.TestCase):
 	def test_runCmd_dir_stdOut(self):
 		returnCode, stdOut, stdErr = winUtility.runCmd("dir")
 		self.assertIn("winUtility.py", stdOut)
+	def test_getServiceStartupAccount_dhcp(self):
+		self.assertEqual(winUtility.getServiceStartupAccount('Dhcp', 'LocalHost'), 'NT Authority\\LocalService')
